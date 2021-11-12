8355b0c735a98755f65d32d726be1294ac5ac37e
Uy Nguyen <NguyenBaoUy@gmail.com>
2021-11-12 05:05:31 -0500

    Initial commit and sharing of the project

---
+++ cmake-build-debug/CMakeFiles/SPV_MonitorTracking.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPV_MonitorTracking.dir/main.cpp.obj"
+  "SPV_MonitorTracking.exe"
+  "SPV_MonitorTracking.exe.manifest"
+  "SPV_MonitorTracking.pdb"
+  "libSPV_MonitorTracking.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPV_MonitorTracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
