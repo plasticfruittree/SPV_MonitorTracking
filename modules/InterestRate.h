213ff0dcb3415fc3de9a3dd05848cef0b1f09989
Uy Nguyen <NguyenBaoUy@gmail.com>
2021-11-20 10:21:24 -0500

    Started on the money market class

--- modules/InterestRate.h
+++ modules/InterestRate.h
@@ -42,13 +42,45 @@ public:
 
     // function
 
-    double rate_to_acr(double rate, double period) {
+    double mrate_to_acr(double rate, double period) {
         return period * log(1+ (rate/period));
     }
 
-    double acr_to_rate(double rate, double period) {
+    double acr_to_mrate(double rate, double period) {
         return period * (exp(rate/period) - 1);
     }
+
+};
+
+class MoneyMarketRates: public InterestRate {
+private:
+    double Addon_rate;
+    double Discount_rate;
+    double Present_value;
+    double Future_value;
+    double Days_in_year;
+
+
+public:
+    // constructors
+    MoneyMarketRate(double value, double addon_rate, double days) {
+        Present_value = value;
+        Future_value = value;
+        Addon_rate = addon_rate;
+        Discount_rate = addon_rate;
+        Days_in_year = days;
+    }
+
+    double Addon_fv()
+    {
+        return present_value + (present_value + addon)
+    }
+
+
+
+
+
+
 
 
 };
