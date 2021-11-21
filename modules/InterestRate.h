ffa3c34c59735db8ce9105e2eff1942df50341cc
Uy Nguyen <NguyenBaoUy@gmail.com>
2021-11-21 05:44:36 -0500

    Made MoneyMarketAddon.

--- modules/InterestRate.h
+++ modules/InterestRate.h
@@ -66,27 +66,101 @@ public:
 
 };
 
-class MoneyMarketRate{
+class MoneyMarketAddon
+{
+
 private:
-    //different rates
-    double Addon_rate, Discount_rate;
-
-    //future and present values
+    double Addon_rate;
     double Present_value, Future_value;
-
-    // annualized period
-    double Days_in_year, Days_til_maturity;
-    double annualized_period;
-
+    double Days_in_year, Days_til_maturity, Annualized_period;
 
 public:
+    // setter and getter
+    double getAddonRate() const {
+        return Addon_rate;
+    }
 
-    // constructors
-    MoneyMarketRate(double rate, double value, double days_in_year, double days_til_maturity)
-    {
+    void setAddonRate(double addonRate) {
+        Addon_rate = addonRate;
+    }
 
+    double getPresentValue() const {
+        return Present_value;
+    }
+
+    void setPresentValue(double presentValue) {
+        Present_value = presentValue;
+    }
+
+    double getFutureValue() const {
+        return Future_value;
+    }
+
+    void setFutureValue(double futureValue) {
+        Future_value = futureValue;
+    }
+
+    double getDaysInYear() const {
+        return Days_in_year;
+    }
+
+    void setDaysInYear(double daysInYear) {
+        Days_in_year = daysInYear;
+    }
+
+    double getDaysTilMaturity() const {
+        return Days_til_maturity;
+    }
+
+    void setDaysTilMaturity(double daysTilMaturity) {
+        Days_til_maturity = daysTilMaturity;
+    }
+
+    double getAnnualizedPeriod() const {
+        return Annualized_period;
+    }
+
+    void setAnnualizedPeriod(double annualizedPeriod) {
+        Annualized_period = annualizedPeriod;
     }
 
 
+    // constructor
+    MoneyMarketAddon(double rate, double value, double days_in_year, double days_til_maturity)
+    {
+        Addon_rate = rate;
+        Present_value = value;
+        Future_value = value;
+        Days_in_year = days_in_year;
+        Days_til_maturity = days_til_maturity;
+        Annualized_period = days_in_year / days_til_maturity;
+    }
 
+
+    // functions
+    double addon_futureValue()
+    {
+        double addon_fv =  Present_value + (Present_value * Addon_rate * Annualized_period);
+        setFutureValue(addon_fv);
+        return addon_fv;
+    }
+
+    double addon_presentValue()
+    {
+        double addon_pv = Future_value / (1 + (Addon_rate * Annualized_period));
+        setPresentValue(addon_pv);
+        return addon_pv;
+    }
+
+    double addon_rate()
+    {
+        double rate_addon = Annualized_period * (Future_value / Present_value);
+        setAddonRate(rate_addon);
+        return rate_addon;
+
+    }
 };
+
+
+
+
